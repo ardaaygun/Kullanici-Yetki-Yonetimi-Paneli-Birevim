8f246107ba7285155e436eb5abba4786edac1a4b
Arda Aygün <ardaayg0@gmail.com>
2026-07-23 16:31:44 +0300

    EF Core altyapısı kuruldu, User ve Role modelleri eklendi

---
+++ KullaniciYonetimi/KullaniciYonetimi/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace KullaniciYonetimi.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; } //Admin,Manager,NormalUser
+       
+        // Bir rolün birden fazla kullanıcısı olabileceğini sisteme söylüyoruz
+        public ICollection<User> Users { get; set; }
+    }
+}
---
+++ KullaniciYonetimi/KullaniciYonetimi/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace KullaniciYonetimi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        // Hash şifreleme.
+        public string PasswordHash { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+
+        public int RoleId { get; set; }
+
+        // (Navigation Property)
+        public Role Role { get; set; }
+    }
+}
